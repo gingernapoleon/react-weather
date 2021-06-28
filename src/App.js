c39b0e079e4b6ed5cf6c9f84228278455d6e2b2f
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-06-28 00:57:17 -0700

    import codesandbox project to VScode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import './App.css';
+import './Weather.css';
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather city="Paris" />
-      </header>
+      <Weather />
     </div>
   );
 }
 
 export default App;
+
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,67 @@
 import React from "react";
 import axios from 'axios';
 
-export default function Weather(props) {
-    function handleResponse(response) {
-    alert (
-        'The weather in ${response.data.name} is ${response.data.main.temp} °C'
-        );
-}
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10
+  };
 
-    let apiKey = "da16df2d631e0a7dbbac5c7b96916177";
-    let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=N${props.city}&appid=${apiKey}&units=metric';
+  return (
+    <div className="Weather">
+      <form className="search-bar">
+        <div className="9">
+          <input
+            type="search"
+            placeholder="Type a city.."
+            className="form-control"
+            autoComplete="off"
+          />
+        </div>
+        <div className="submit-button">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </form>
 
-    axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>;
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} m/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
